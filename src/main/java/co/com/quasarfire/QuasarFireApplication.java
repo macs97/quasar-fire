6130c22bb42354719c2f07a05d4ba3089298a42c
Marcela Cardona <mcardona@rapicredit.com>
2022-03-03 20:26:52 -0500

    Add base dependencies and add MainApplication and application.properties

---
+++ src/main/java/co/com/quasarfire/QuasarFireApplication.java
@@ -0,0 +1,12 @@
+package co.com.quasarfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuasarFireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuasarFireApplication.class, args);
+    }
+}
