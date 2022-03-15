3d007788fc615913431d7fecee9be9048efa6735
Marcela Cardona <mcardona@rapicredit.com>
2022-03-15 02:47:24 -0500

    Add logs to controller

--- src/main/java/co/com/quasarfire/controller/CommandCenterController.java
+++ src/main/java/co/com/quasarfire/controller/CommandCenterController.java
@@ -6,6 +6,7 @@ import co.com.quasarfire.domain.request.SatellitesRequest;
 import co.com.quasarfire.domain.response.CommandCenterResponse;
 import io.swagger.annotations.ApiOperation;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class CommandCenterController {
 
     private final CommandCenterBusiness commandCenter;
@@ -29,6 +31,7 @@ public class CommandCenterController {
     @ApiOperation(value = "Encuentra la posicion de la nave respecto la distancia de los 3 satelites y devuelve mensaje.", httpMethod = "POST", response = CommandCenterResponse.class)
     public ResponseEntity<CommandCenterResponse> getSpaceshipPositionAndMessage(
         @RequestBody SatellitesRequest satellitesRequest) {
+        log.info("Start TOPSECRET");
         return new ResponseEntity<>(this.commandCenter.getSpaceshipPositionAndMessage(satellitesRequest),
             HttpStatus.OK);
     }
@@ -37,7 +40,7 @@ public class CommandCenterController {
     @ApiOperation(value = "Encuentra la posicion de la nave la distancia de 1 satelite y devuelve mensaje por metodo Get. Sino tiene mensaje devuelve 'No hay suficiente informacion'", httpMethod = "GET", response = CommandCenterResponse.class)
     public ResponseEntity<CommandCenterResponse> getPositionAndMessageBySpaceship(@PathVariable String name,
         @RequestBody SatelliteDetail satelliteDetail) {
-
+        log.info("Start TOPSECRET_SPLIT GET");
         CommandCenterResponse commandCenterResponse = this.commandCenter.getSpaceshipPositionAndMessage(
             SatellitesRequest.builder().satellites(List.of(
                 SatelliteDetail.builder().name(name).distance(satelliteDetail.getDistance())
@@ -53,6 +56,7 @@ public class CommandCenterController {
     @ApiOperation(value = "Encuentra la posicion de la nave la distancia de 1 satelite y devuelve mensaje por metodo Post.", httpMethod = "POST", response = CommandCenterResponse.class)
     public ResponseEntity<CommandCenterResponse> postPositionAndMessageBySpaceship(@PathVariable String name,
         @RequestBody SatelliteDetail satelliteDetail) {
+        log.info("Start TOPSECRET_SPLIT POST");
         SatellitesRequest satellitesRequest = SatellitesRequest.builder().satellites(List.of(
             SatelliteDetail.builder().name(name).distance(satelliteDetail.getDistance())
                 .message(satelliteDetail.getMessage()).build())).build();
@@ -63,6 +67,7 @@ public class CommandCenterController {
     @GetMapping("health_check")
     @ApiOperation(value = "Metodo para verificar que la aplicacion inicia correctamente", httpMethod = "GET", response = String.class)
     public ResponseEntity<String> healthCheck() {
+        log.info("Start HEALTH CHECK");
         return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 }
